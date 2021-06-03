abf523bf055555622fba0cf8a3810fbb7c32012e
Arpita Naval <navalarpita@gmail.com>
2021-06-03 22:10:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import Header from "./components/Header";
 import Note from "./components/Note";
 import Footer from "./components/Footer";
-
+import notes from "./notes.js";
 
 function App() {
   return (
     <div>
     <Header />
-    <Note />
+  {notes.map(note = {
+    <Note
+      key={note.key}
+      noteTitle={note.title}
+      noteContent={note.content}
+       />
+    })}
+    <Note  />
+
     <Footer />
 
     </div>
