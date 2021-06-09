eaf4b7179de2367f3a2693bbb1ef9831a76ced32
Arpita Naval <navalarpita@gmail.com>
2021-06-09 20:29:14 +0530

    Add file CreateArea.jsx

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -3,7 +3,13 @@ import React from "react";
 function CreateArea(){
   return (
     <div>
-    
+    <from>
+    <input name="title" placeholder="Title" />
+       <textarea name="content" placeholder="Take a note..." rows="3" />
+       <button>Add</button>
+
+    </from>
+
     </div>
   );
 
