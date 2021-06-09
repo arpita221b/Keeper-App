b3ae93668e827d509df59afce98567b6980e9ee4
Arpita Naval <navalarpita@gmail.com>
2021-06-09 22:27:33 +0530

    Update App,js

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -2,9 +2,10 @@ import React from "react";
 function CreateArea(){
   return (
     <div className = "addNote">
-    <input name="title" placeholder="Title" />
-    <textarea name="content" placeholder="Take a note..." rows="3" />
+     <form> <input name="title" placeholder="Title" />
+     <textarea name="content" placeholder="Take a note..." rows="3" />
      <button><i class="fa fa-plus"></i> </button>
+     </form>
 
     </div>
   );
