50101efced5da876e03a95bfbff2eead14aef8e5
Arpita Naval <navalarpita@gmail.com>
2021-06-10 10:20:21 +0530

    Add State in CreateArea.jsx

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
 function CreateArea(){
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event){
+    const { name, value} = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
   return (
     <div className = "addNote">
-     <form> <input name="title" placeholder="Title" />
-     <textarea name="content" placeholder="Take a note..." rows="3" />
+     <form> <input
+     name="title"
+     onChange={handleChange}
+     value={note.title}
+     placeholder="Title" />
+
+     <textarea
+     name="content"
+     onChange={handleChange}
+     value={note.content}
+     placeholder="Take a note..."
+      rows="3" />
      <button><i class="fa fa-plus"></i> </button>
      </form>
 
